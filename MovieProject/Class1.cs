6fccaf2fd7c5bf0de6c0712651d0776a02de2979
tmoncelle87 <tmoncelle87@gmail.com>
2024-02-06 18:50:32 -0600

    updated movies

---
+++ MovieProject/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+    public class StudentInformation
+{//creation of variables with gets and sets
+    private string[] techID = new string[8];
+    private string classNumber1Name = "n/a";
+    private string classNumber2Name = "n/a";
+    private string classNumber3Name = "n/a";
+    private string classNumber4Name = "n/a";
+    private string classNumber5Name = "n/a";
+    private string classNumber1Times = "n/a";
+    private string classNumber2Times = "n/a";
+    private string classNumber3Times = "n/a";
+    private string classNumber4Times = "n/a";
+    private string classNumber5Times = "n/a";
+    private string classNumber1Days = "n/a";
+    private string classNumber2Days = "n/a";
+    private string classNumber3Days = "n/a";
+    private string classNumber4Days = "n/a";
+    private string classNumber5Days = "n/a";
+    private string extraCurricularDays = "n/a";
+    private string extraCurricularTimes = "n/a";
+    private int jobWeeklyHours = 0;
+    private bool hasJob = false;
+    private bool hasExtraCurriculars = false;
+    private bool hasFiveClasses = false;
+
+
+}
+}
